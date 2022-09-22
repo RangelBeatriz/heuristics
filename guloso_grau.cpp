cf15c8c9136bf5bba40618457fc27fa59a4c431d
Beatriz Rangel <srangel.beatriz@gmail.com>
2022-09-21 22:03:10 -0300

    Adicionando arquivos de grau

---
+++ guloso_grau.cpp
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#pragma warning(disable: 4996)
+#pragma warning(disable: 4661)
+#pragma warning(disable: 4267)
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <array>
+#include <unordered_map>
+#include <list>
+#include <queue>
+#include <string>
+#include <random>
+#include <algorithm>
+#include <iterator>
+#include <climits>
+#include <chrono>
+#include <atomic>
+#include <thread>
+#include <sstream>
+#include <functional>
+#include <iostream>
+#include <fstream> 
+#include <condition_variable>
+#include <mutex>
+#include<iomanip>
+#include<stdexcept>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+
+#include "BibGrafos/BibGrafos.h"
+
+#ifndef DATADIR
+#ifdef ILO_WINDOWS
+#define DIRSEP "\\"
+#else
+#define DIRSEP "/"
+#endif
+#define DATADIR ".." DIRSEP ".."  DIRSEP ".." DIRSEP ".." DIRSEP "opl" DIRSEP
+#endif
+
+using namespace std;
+using namespace std::chrono;
+using namespace BibGrafos;
+
+int main()
+{
+	
+	 Grafo G = BibGrafos::Grafo::ConstruirDeArquivoBinario("n2000p50_G2.gbin");
+
+	int k = G.ObterGrauMinimo()/2;
+	VetorRequisitos R = VetorRequisitos::GerarConstante(&G,k);
+	VetorRequisitos Raux = R; 
+
+	set<Vertice> faltaDominar(G.Vertices.begin(), G.Vertices.end());
+	set<Vertice> S;
+	int grau[G.N()];
+
+	for (Vertice v: faltaDominar){
+		grau[v.Indice()] = G.Grau(v);
+	}
+
+	auto start = high_resolution_clock::now();
+	while (faltaDominar.size() > 0)
+	{
+		Vertice v = *faltaDominar.begin();
+		for(Vertice w: faltaDominar){
+			if(grau[v.Indice()] < grau[w.Indice()]){
+				v = w;
+			}
+		}
+		
+		S.insert(v);
+
+		for(Vertice u : G.Adjacentes(v))
+		{
+			Raux[u] = Raux[u] - 1;
+			grau[u.Indice()] = grau[u.Indice()] - 1;
+			if(Raux[u] == 0){
+				faltaDominar.erase(u);
+			}
+		}
+
+		faltaDominar.erase(v);
+		//G->RemoverVertice(v);		
+	}
+	auto stop = high_resolution_clock::now();
+
+	auto duration = duration_cast<microseconds>(stop-start);
+	
+	cout << "Tempo em microsegundos: " << duration.count() << endl;
+	cout << "Tamanho:" << S.size() << endl;
+
+	
+	
+	return 0;	
+}
